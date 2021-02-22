9d9b4cf20be34c0c73ed1c4f199338604d6589b1
Javier Moncada <javierandresmd@ufps.edu.co>
2021-02-21 21:28:03 -0500

    Metodo subscribe()

--- src/main/java/com/ufps/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/ufps/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,5 +1,7 @@
 package com.ufps.springboot.reactor.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,17 +11,28 @@ import reactor.core.publisher.Flux;
 @SpringBootApplication
 public class SpringBootReactorApplication implements CommandLineRunner {
 
+	private static final Logger log = LoggerFactory.getLogger(SpringBootReactorApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactorApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		Flux<String> nombres = Flux.just("Andres", "Camilo", "Juan", "Javier")
-				.doOnNext(System.out::println);
-		
-		nombres.subscribe();
-		
+		Flux<String> nombres = Flux.just("Andres", "Camilo","", "Juan", "Javier").doOnNext(e -> {
+			if (e.isEmpty()) {
+					throw new RuntimeException("Los nombre no pueden ser vacios");
+			}
+			
+				System.out.println(e);
+
+			
+		});
+
+		nombres.subscribe(e -> log.info(e),
+				error -> log.error(error.getMessage())
+				);
+
 	}
 
 }
